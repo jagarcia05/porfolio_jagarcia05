6ad52b44e841ddce72383dc19e0724e1516c9a9c
jagarcia05 <joseantoniogarciapascual17@gmail.com>
2024-12-11 13:39:31 +0100

    pepe

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from "path";
 export default defineConfig({
   root: "./src", // no olvidemos cambiar la raíz del proyecto a "src"
   build: {
+    outDir: "../docs", // Aquí debe estar
     rollupOptions: {
       input: {
         main: resolve(__dirname, "./src/index.html"), // Página principal
@@ -11,5 +12,5 @@ export default defineConfig({
       },
     },
   },
-  base: "./", // Opcional: rutas relativas para mayor compatibilidad
+  base: "/porfolio_jagarcia05", // Opcional: rutas relativas para mayor compatibilidad
 });
